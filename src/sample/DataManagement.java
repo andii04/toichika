92abb930cae0eb4146929d4ab3d2f74ab07544c1
andii04 <36232071+andii04@users.noreply.github.com>
2020-05-25 11:28:45 +0200

    Maven jacksonlib eingebunden

---
+++ src/sample/DataManagement.java
@@ -0,0 +1,13 @@
+package sample;
+
+import com.fasterxml.jackson.databind.util.JSONPObject;
+
+public class DataManagement {
+    //private Object Cells;
+
+    public void ReadAllDataFromJSON(JSONPObject jsonpObject){
+        //ObjectMapper om = new ObjectMapper();
+        //ObjectMapper cl = new ObjectMapper().readerFor(Cells);
+        
+    }
+}
